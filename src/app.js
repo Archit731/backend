d073b3eeb232934c91e51c9974bf0da79f1ed664
Archit731 <archit.bhatnagar31@gmail.com>
2024-04-08 14:39:55 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: true,
+  })
+);
 
-}))
+app.use(express.json({ limit: "16kb" })); //for json se data aana
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //use of extended is to give objects inside objects
+app.use(express.static("public")); //for any file pdf that comes and to store them in server like public assets so any can see and access
+app.use(cookieParser());
 
-app.use(express.json({limit: "16kb"})) //for json se data aana
-app.use(express.urlencoded({extended: true, limit: "16kb"}))  //use of extended is to give objects inside objects 
-app.use(express.static("public")) //for any file pdf that comes and to store them in server like public assets so any can see and access
-app.use((cookieParser()))
+//routes import
+import userRouter from "./routes/user.routes.js";
 
+//routes declaration
+app.use("/api/v1/users", userRouter); //http://localhost:8000/api/v1/users/register or l/ogin
 
-
-export { app }
+export { app };
